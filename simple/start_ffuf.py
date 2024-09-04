b64afdb7d30dcbabe6899da1f921f0d68f7abdf4
strongreen <strongreen@live.com>
2024-09-04 11:33:42 -0300

    corrigindo versão simples

--- simple/start_ffuf.py
+++ simple/start_ffuf.py
@@ -2,13 +2,12 @@ import subprocess
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
 
-def start_ffuf(entry_url, entry_wordlist, method_var,header_var, log_area):
+def start_ffuf(entry_url, entry_wordlist, method_var, log_area):
     target_url = entry_url.get()
     wordlist = entry_wordlist.get()
     method = method_var.get()
-    header = header_var.get()
 
-    command = f"ffuf -u {target_url} -w {wordlist} -X {method} -H {header}"
+    command = f"ffuf -u {target_url} -w {wordlist} -X {method}"
     process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     
     log_area.delete(1.0, END)
@@ -16,3 +15,4 @@ def start_ffuf(entry_url, entry_wordlist, method_var,header_var, log_area):
     for line in process.stdout:
         log_area.insert(END, line.decode())
         log_area.see(END)
+        log_area.update_idletasks()
